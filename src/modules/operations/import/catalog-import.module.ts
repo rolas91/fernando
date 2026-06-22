2ad2a894d51fa9aca9757ed413ececc7f812d1ed
rsanchez <rsanchez@sanmartin.com.ni>
2026-06-22 00:16:28 -0600

    ultims cambios

--- src/modules/operations/import/catalog-import.module.ts
+++ src/modules/operations/import/catalog-import.module.ts
@@ -4,6 +4,7 @@ import { Certification } from '../../../entities/certification.entity';
 import { Client } from '../../../entities/client.entity';
 import { CommercialCatalogItem } from '../../../entities/commercial-catalog-item.entity';
 import { Equipment } from '../../../entities/equipment.entity';
+import { FormTemplate } from '../../../entities/form-template.entity';
 import { Material } from '../../../entities/material.entity';
 import { ProjectType } from '../../../entities/project-type.entity';
 import { Skill } from '../../../entities/skill.entity';
@@ -12,12 +13,17 @@ import { WorkOrderType } from '../../../entities/work-order-type.entity';
 import { Worker } from '../../../entities/worker.entity';
 import { WorkerCertification } from '../../../entities/worker-certification.entity';
 import { WorkerRole } from '../../../entities/worker-role.entity';
+import { AccessModule } from '../../access/access.module';
+import { AuthModule } from '../../auth/auth.module';
 import { RealtimeModule } from '../../realtime/realtime.module';
 import { CatalogImportController } from './catalog-import.controller';
 import { CatalogImportService } from './catalog-import.service';
+import { OperationsAuthGuard } from '../operations-auth.guard';
 
 @Module({
   imports: [
+    AccessModule,
+    AuthModule,
     RealtimeModule,
     TypeOrmModule.forFeature([
       Skill,
@@ -32,10 +38,11 @@ import { CatalogImportService } from './catalog-import.service';
       Client,
       Worker,
       WorkerCertification,
+      FormTemplate,
     ]),
   ],
   controllers: [CatalogImportController],
-  providers: [CatalogImportService],
+  providers: [CatalogImportService, OperationsAuthGuard],
   exports: [CatalogImportService],
 })
 export class CatalogImportModule {}
